b4d4498c5bb72e3ea5fb0534f2611b1434afbc87
Ludwig Muench <muench.ludwig@gmail.com>
2019-03-22 10:26:09 -0500

    update readme

--- cli/fs/fs.go
+++ cli/fs/fs.go
@@ -34,21 +34,23 @@ func Ls() {
 		log.Fatal(err)
 	}
 
-	for id, filename := range filenames {
+	plugCnt := 0
+	for _, filename := range filenames {
 		if filepath.Ext(filename) == ".go" {
+			plugCnt++
 			name := filename[:len(filename)-3]
 			data, err := ioutil.ReadFile(name + ".json")
+			fmt.Println(plugCnt, "plugin:", name)
 			if err != nil {
-				fmt.Print(id, " ", name, ": metadata (", name, ".json) is missing!\n")
+				fmt.Print("↳ error: metadata (", name, ".json) is missing!\n")
 			} else {
 				var metadata PlugMetadata
 				err = json.Unmarshal(data, &metadata)
 				if err != nil {
-					fmt.Println(id, name, ": metadata parsing error ", err)
+					fmt.Print("↳ error: metadata parsing error (", err, ")\n")
 				}
-				fmt.Println(id, name)
-				for n, ias := range metadata.Services {
-					fmt.Print("↳ ", id, ".", n+1, " interface: ", ias.IFace, ", symbol: ", ias.Symb, "\n")
+				for _, ias := range metadata.Services {
+					fmt.Print("↳ interface: ", ias.IFace, ", symbol: ", ias.Symb, "\n")
 				}
 			}
 		}
