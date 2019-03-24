1fbb07f912ab3e1c4043cc2ea554f719a651cc10
Ludwig Muench <muench.ludwig@gmail.com>
2019-03-24 18:14:20 -0500

    finish example app

--- examples/greeter/main.go
+++ examples/greeter/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"log"
+
 	"github.com/lmuench/plg/rpc/server"
 
 	"github.com/lmuench/plg/plg"
 )
 
+type Greeter interface {
+	Greet(name string)
+}
+
 func main() {
 	plg := plg.NewPLG()
-	server.Run(plg)
+	ch := make(chan error)
+	go server.Run(plg, ch)
+
+	err := <-ch
+	if err != nil {
+		log.Fatal(err)
+	}
+	symb, ok := plg.GetSymbol("Greeter")
+	if !ok {
+		log.Fatal("registry does not contain a Greeter service")
+	}
+	greeter, ok := symb.(Greeter)
+	if ok {
+		greeter.Greet("world")
+	}
 }
--- rpc/server/server.go
+++ rpc/server/server.go
@@ -16,24 +16,26 @@ const (
 
 type server struct {
 	plg *plg.PLG
+	ch  chan error
 }
 
 func (s *server) RegisterPlugin(ctx context.Context, in *pb.Plugin) (*pb.Error, error) {
 	log.Printf("Received absolute object path: %s", in.AbsObjPath)
 	err := s.plg.RegisterPlugin(in.AbsObjPath)
+	s.ch <- err
 	if err != nil {
 		return nil, err
 	}
 	return &pb.Error{Msg: "ok"}, nil
 }
 
-func Run(plg *plg.PLG) {
+func Run(plg *plg.PLG, ch chan error) {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterRegistryServer(s, &server{plg: plg})
+	pb.RegisterRegistryServer(s, &server{plg: plg, ch: ch})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
